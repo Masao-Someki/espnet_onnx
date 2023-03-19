63035abd80ea85374a380c9f4283c7e0fbad64d2
Masao-Someki <masao.someki@gmail.com>
2023-03-20 02:46:24 +0900

    Modified benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -11,15 +11,21 @@ do
 	do
 		output_dir=tools/benchmark/result/${onnx_type}
 		mkdir -p ${output_dir}/${model_type}
-		python benchmark.py \
-			tools.benchmark.config.${onnx_type}.${model_type} \
-			${output_dir}/${model_type}
+		# python benchmark.py \
+		# 	tools.benchmark.config.${onnx_type}.${model_type} \
+		# 	${output_dir}/${model_type}
 
-		# sclite \
-		# 	-r ${output_dir}/${model_type}/ref.txt \
-		# 	-h ${output_dir}/${model_type}/espnet_hypo.txt \
-		# 	-i rm \
-		# 	-o all stdout > ${output_dir}/${model_type}/espnet_wer.txt
+		sclite \
+			-r ${output_dir}/${model_type}/ref.txt \
+			-h ${output_dir}/${model_type}/espnet_hypo.txt \
+			-i rm \
+			-o all stdout > ${output_dir}/${model_type}/espnet_wer.txt
+
+		sclite \
+			-r ${output_dir}/${model_type}/ref.txt \
+			-h ${output_dir}/${model_type}/onnx_hypo.txt \
+			-i rm \
+			-o all stdout > ${output_dir}/${model_type}/onnx_wer.txt
 		# python summarize.py \
 		# 	${output_dir}/${model_type}/espnet_rtf.txt # > ${output_dir}/${model_type}/espnet_rtf.txt
 		# python summarize.py \
